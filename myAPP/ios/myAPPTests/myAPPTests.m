3a3e2c56f43e9dd9b8b176c4d52a69523dada2e9
SheepHan <williamcheng0928@gmail.com>
2018-10-23 16:52:30 +0800

    hello word

--- myAPP/ios/myAPPTests/myAPPTests.m
+++ myAPP/ios/myAPPTests/myAPPTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface myAPPTests : XCTestCase
+@interface myAppTests : XCTestCase
 
 @end
 
-@implementation myAPPTests
+@implementation myAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
