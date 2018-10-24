d70fe48a7ee2f18683c025345fe2aea0285a68a7
SheepHan <williamcheng0928@gmail.com>
2018-10-24 22:19:13 +0800

    add firstApp

--- myAPP/App.js
+++ myAPP/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
   render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-        <View>
-          <Text>Hello world!</Text>
-        </View>
+      <Text>{display}</Text>
     );
   }
 }
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='just red' style={styles.red}/>
+        <Blink text='just bigblue' style={styles.bigblue}/>
+        <Blink text='bigblue, then red' style={[styles.bigblue, styles.red]}/>
+        <Blink text='red, then bigblue' style={[styles.red, styles.bigblue]}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
