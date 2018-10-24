a3c2311e07b5ef449f51d4dffb36bd6dcd602448
SheepHan <williamcheng0928@gmail.com>
2018-10-24 23:53:40 +0800

    add

--- fisrtApp/App.js
+++ fisrtApp/App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import HelloComponent from './HelloComponent'
+import LifecircleComponent from './LifecircleComponent'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,9 +26,10 @@ export default class App extends Component<Props> {
         {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text> */}
-        <HelloComponent 
+        {/* <HelloComponent 
         name="小明"
-        />
+        /> */}
+        <LifecircleComponent/>
       </View>
     );
   }
--- fisrtApp/LifecircleComponent.js
+++ fisrtApp/LifecircleComponent.js
@@ -13,22 +13,38 @@ import {StyleSheet, Text, View} from 'react-native';
 export default class LifecircleComponent extends Component{
   constructor(props) { //初始化组件
     super(props); //初始化Component
+    console.log("constructor")
   }
   componentWillMount() {
-
+    console.log("componentWillMount")
   }
 
   componentDidMount() {
-
+    console.log("componentDidMount")
   }
 
   componentWillReceiveProps(nextProps) {
-
+    console.log("componentWillReceiveProps")
   }
 
   shouldComponentUpdate(nextProps,nextState) {
+    console.log("shouldComponentUpdate")
     return true
   }
+
+  componentWillUpdate(nextProps,nextState) {
+    console.log("componentWillUpdate")
+  }
+
+  componentDidUpdate(nextProps,nextState) {
+    console.log("componentDidUpdate")
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+  }
+
+
   render() {
     return <Text style={{fontSize:40,backgroundColor:'red',color:'#fff'}}>hello{this.props.name}</Text>
   }
