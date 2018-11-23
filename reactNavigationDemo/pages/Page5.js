73616e872d7e73471109c4a9c29e43cd9405254a
SheepHan <williamcheng0928@gmail.com>
2018-11-23 23:34:03 +0800

    add

--- reactNavigationDemo/pages/Page5.js
+++ reactNavigationDemo/pages/Page5.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'green',
+    // backgroundColor: 'green',
   },
   welcome: {
     fontSize: 20,
