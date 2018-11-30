d56bc2c81c6a37e71643312aaa8ac9a40ee289c6
SheepHan <williamcheng0928@gmail.com>
2018-11-30 20:59:58 +0800

    add

--- reactNavigationDemo/pages/Page5.js
+++ reactNavigationDemo/pages/Page5.js
@@ -47,12 +47,12 @@ export default class Page5 extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   backgroundColor: 'green',
-  // },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
