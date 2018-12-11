f79a4c3ecdf4ec59483227fa7e93bc85d63f6d4f
SheepHan <williamcheng0928@gmail.com>
2018-12-11 22:47:43 +0800

    add

--- fisrtApp/FlexBoxTest.js
+++ fisrtApp/FlexBoxTest.js
@@ -35,13 +35,13 @@ export default class FlexBox extends Component{
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
